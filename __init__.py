fcdc637db6c70d216f1b390034b6fab0698fd9a3
Ygor Azambuja <ygorazambuja@gmail.com>
2021-11-02 16:09:54 -0400

    change app name

--- __init__.py
+++ __init__.py
@@ -44,7 +44,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler("cat", cat))
 
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-    updater.bot.set_webhook(f"https://draco-bot.herokuapp.com/{TOKEN}")
+    updater.bot.set_webhook(f"https://draco-but.herokuapp.com/{TOKEN}")
 
     updater.start_polling()
     updater.idle()
